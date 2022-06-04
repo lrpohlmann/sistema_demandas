ef89419c1e2bca301af300cd2b07c53171324f3a
Leonardo <leo.pohlmann@hotmail.com>
2022-06-03 23:41:46 -0300

    refatoração

--- sistema/model/operacoes/demanda.py
+++ sistema/model/operacoes/demanda.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from typing import Protocol, Sequence
 from sistema.model.entidades.fato import FatoTarefaFinalizada
 from sistema.model.entidades.tarefa import StatusTarefa
 from sistema.model.operacoes.tarefa import set_status
@@ -8,13 +9,22 @@ class TarefaNaoEncontradaException(Exception):
     pass
 
 
-def finalizar_tarefa_da_demanda(id_tarefa, demanda):
+class _TemTarefas(Protocol):
+    tarefas: Sequence
+
+
+def get_tarefa(id_tarefa: int, demanda: _TemTarefas):
     for t in demanda.tarefas:
         if t.id_tarefa == id_tarefa:
-            tarefa = set_status(t, StatusTarefa.FINALIZADA)
-            demanda.linha_do_tempo.sequencia_de_fatos.append(
-                FatoTarefaFinalizada("", datetime(2022, 5, 1, 15, 0))
-            )
-            return demanda
+            return t
 
     raise TarefaNaoEncontradaException()
+
+
+def finalizar_tarefa_da_demanda(id_tarefa, demanda):
+    t = get_tarefa(id_tarefa, demanda)
+    tarefa = set_status(t, StatusTarefa.FINALIZADA)
+    demanda.linha_do_tempo.sequencia_de_fatos.append(
+        FatoTarefaFinalizada("", datetime(2022, 5, 1, 15, 0))
+    )
+    return demanda
