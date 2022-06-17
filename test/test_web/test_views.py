554cc8ed19aebcb92936336132743d5e71d1782f
Leonardo <leo.pohlmann@hotmail.com>
2022-06-17 20:16:33 -0300

    ok

--- test/test_web/test_views.py
+++ test/test_web/test_views.py
@@ -3,6 +3,6 @@ from flask.testing import FlaskClient
 from test.test_web.fixtures import client, web_app
 
 
-def test_main_view(client: FlaskClient):
+def test_main_view_status(client: FlaskClient):
     resposta = client.get("/")
     assert resposta.status == "200 OK"
