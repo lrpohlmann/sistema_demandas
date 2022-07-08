c518128646c5f4ff3e5fd40b581fb0fd476b3bb4
Leonardo <leo.pohlmann@hotmail.com>
2022-07-08 11:02:58 -0300

    views inutilizadas

--- sistema/web/views/__init__.py
+++ sistema/web/views/__init__.py
@@ -4,12 +4,12 @@ from typing import Tuple
 from flask import Flask
 from sqlalchemy.orm import scoped_session
 
-from sistema.web.views import demanda, forms, home, usuario
+from sistema.web.views import demanda, forms, home
 
 
 def setup_todas_views(app: Flask, db: scoped_session) -> Tuple[Flask, scoped_session]:
     return reduce(
         lambda args, setup_views: setup_views(*args),
-        [demanda.setup_views, forms.setup_views, home.setup_views, usuario.setup_views],
+        [demanda.setup_views, forms.setup_views, home.setup_views],
         (app, db),
     )
