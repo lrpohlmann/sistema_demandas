eeec0141767ec0ad12bf8fa5280b4a6d31bfb80d
Leonardo <leo.pohlmann@hotmail.com>
2022-07-19 09:50:16 -0300

    finalizar tarefa view

--- sistema/web/views/tarefa.py
+++ sistema/web/views/tarefa.py
@@ -1,7 +1,8 @@
 from flask import Flask
 from sqlalchemy.orm import scoped_session
 
-from sistema.model.entidades.tarefa import Tarefa
+from sistema.model.entidades.tarefa import Tarefa, StatusTarefa
+from sistema.model.operacoes.tarefa import set_status
 
 
 def setup_views(app: Flask, db: scoped_session):
@@ -10,6 +11,16 @@ def setup_views(app: Flask, db: scoped_session):
         tarefa = db.get(Tarefa, tarefa_id)
         if tarefa:
             db.delete(tarefa)
+            db.commit()
+            return "", 200
+        else:
+            return "", 404
+
+    @app.route("/tarefa/status/finalizar/<int:tarefa_id>", methods=["PUT"])
+    def finalizar_tarefa_view(tarefa_id: int):
+        tarefa = db.get(Tarefa, tarefa_id)
+        if tarefa:
+            set_status(tarefa, StatusTarefa.FINALIZADA)
             db.commit()
             return "", 200
         else:
--- test/test_web/test_views/test_tarefa.py
+++ test/test_web/test_views/test_tarefa.py
@@ -1,5 +1,5 @@
 from sistema.model.entidades.demanda import Demanda, TipoDemanda
-from sistema.model.entidades.tarefa import Tarefa
+from sistema.model.entidades.tarefa import Tarefa, StatusTarefa
 from sistema.model.entidades.usuario import Usuario
 from test.test_web.fixtures import web_app
 
@@ -15,3 +15,16 @@ def test_deletar_tarefa(web_app):
 
     demanda_sem_tarefa = web_app["db"].get(Demanda, 1)
     assert len(demanda_sem_tarefa.tarefas) == 0
+
+
+def test_finalizar_status_tarefa(web_app):
+    tarefa = Tarefa(titulo="Z")
+    demanda = Demanda(titulo="X", tipo=TipoDemanda("Y"), tarefas=[tarefa])
+    web_app["db"].add(demanda)
+    web_app["db"].commit()
+
+    response = web_app["client"].put("/tarefa/status/finalizar/1")
+    assert response.status_code == 200
+
+    tarefa_atualizada = web_app["db"].get(Tarefa, 1)
+    assert tarefa_atualizada.status == StatusTarefa.FINALIZADA
