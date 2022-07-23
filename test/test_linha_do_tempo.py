77d0ec2e6181292a26e2eb1552f7c33cb5570345
Leonardo <leo.pohlmann@hotmail.com>
2022-07-23 16:51:45 -0300

    inserir_fatos atualizado

--- test/test_linha_do_tempo.py
+++ test/test_linha_do_tempo.py
@@ -27,10 +27,16 @@ class FatoProtocol(Protocol):
     tipo: str
 
 
+@runtime_checkable
+class _TemSequenciaDeFatos(Protocol):
+    fatos: Sequence[FatoProtocol]
+
+
 def inserir_fatos(
-    sequencia_de_fatos: Sequence[FatoProtocol], *fatos: FatoProtocol
-) -> Sequence[FatoProtocol]:
-    return sorted([*sequencia_de_fatos, *fatos], key=lambda f: f.data_hora)
+    tem_fatos: _TemSequenciaDeFatos, *fatos: FatoProtocol
+) -> _TemSequenciaDeFatos:
+    tem_fatos.fatos = sorted([*tem_fatos.fatos, *fatos], key=lambda f: f.data_hora)
+    return tem_fatos
 
 
 def test_instanciar_tarefa():
@@ -70,27 +76,6 @@ def test_adicionar_tarefa_na_demanda():
     assert demanda.tarefas == [tarefa1, tarefa2]
 
 
-def test_inserir_fato_na_sequencia_de_fatos():
-    fato1: FatoProtocol = SimpleNamespace(
-        data_hora=datetime(2022, 1, 1, 10, 5), tipo="Basico"
-    )
-    fato2: FatoProtocol = SimpleNamespace(
-        data_hora=datetime(2021, 12, 25, 20, 15), tipo="Basico"
-    )
-    fato3: FatoProtocol = SimpleNamespace(
-        data_hora=datetime(2022, 3, 12, 9, 5), tipo="Basico"
-    )
-
-    seq_fatos = []
-    seq_fatos_nova = inserir_fatos(
-        inserir_fatos(inserir_fatos(seq_fatos, fato2), fato1), fato3
-    )
-
-    assert seq_fatos_nova[0] == fato2
-    assert seq_fatos_nova[1] == fato1
-    assert seq_fatos_nova[2] == fato3
-
-
 def test_insercao_de_fatos_especificos():
     fato1 = Fato(
         tipo=TipoFatos.SIMPLES,
@@ -106,11 +91,11 @@ def test_insercao_de_fatos_especificos():
         descricao="Envio documento de reposta",
     )
 
-    seq_fatos = []
-    nova_seq_fatos = inserir_fatos(seq_fatos, fato2, fato1)
+    tem_seq_fatos = SimpleNamespace(fatos=[])
+    nova_seq_fatos = inserir_fatos(tem_seq_fatos, fato2, fato1)
 
-    assert nova_seq_fatos[0] == fato1
-    assert nova_seq_fatos[1] == fato2
+    assert nova_seq_fatos.fatos[0] == fato1
+    assert nova_seq_fatos.fatos[1] == fato2
 
 
 def test_finalizar_tarefa_de_uma_demanda():
