9a4552b2b1291616e2378f568142cccc25b8334f
Leonardo <leo.pohlmann@hotmail.com>
2022-08-04 14:48:04 -0300

    upload folder test config

--- sistema/web/configs/config_teste.py
+++ sistema/web/configs/config_teste.py
@@ -1,6 +1,12 @@
+import tempfile
+from pathlib import Path
+
 import flask_wtf
+
 
 DB = "sqlite+pysqlite:///:memory:"
 TESTING = True
 SECRET_KEY = "dev"
 WTF_CSRF_ENABLED = False
+_UPLOAD_FOLDER_OBJECT = tempfile.TemporaryDirectory()
+UPLOAD_FOLDER = Path(_UPLOAD_FOLDER_OBJECT.name)
