926f631012000942b756b19aecf19251c5665f37
Leonardo <leo.pohlmann@hotmail.com>
2022-09-05 23:40:38 -0300

    fixure db separado

---
+++ test/fixtures.py
@@ -0,0 +1,11 @@
+import pytest
+
+from sistema.persistencia import setup_persistencia
+
+
+@pytest.fixture
+def temp_db():
+    db, reg, meta = setup_persistencia("sqlite+pysqlite:///:memory:")
+    yield db
+    reg.dispose()
+    db.remove()
