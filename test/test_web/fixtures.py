8d439701f4266cdba8ca04f4adff35af3f5acf8e
lrpohlmann <leo.pohlmann@hotmail.com>
2022-09-23 23:07:13 -0300

    atualizar setup

--- test/test_web/fixtures.py
+++ test/test_web/fixtures.py
@@ -6,6 +6,7 @@ from sistema.web.app import (
     _setup_app_db,
     _setup_web_app,
     criar_web_app,
+    _setup_app_autenticacao,
 )
 from sistema.web.views import setup_todas_views
 
@@ -17,6 +18,7 @@ TEST_DB_CAMINHO = "sqlite+pysqlite:///:memory:"
 def web_app():
     app = _setup_web_app(test_config=True)
     db, mapper, metadata = _setup_app_db(app)
+    _setup_app_autenticacao(app, db)
     setup_todas_views(app, db)
     client = app.test_client()
 
