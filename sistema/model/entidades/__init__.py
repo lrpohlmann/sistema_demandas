9e04d351e35106432a8398b91d7dc5c8fe5bc429
lrpohlmann <leo.pohlmann@hotmail.com>
2022-10-07 22:46:25 -0300

    package imports

---
+++ sistema/model/entidades/__init__.py
@@ -0,0 +1,5 @@
+from .demanda import Demanda, TipoDemanda
+from .documento import Documento
+from .fato import Fato, TipoFatos
+from .tarefa import Tarefa, StatusTarefa
+from .usuario import Usuario
