407a52bf5c8e2f2bd314ccf1c234c331a2ffeeae
lrpohlmann <leo.pohlmann@hotmail.com>
2022-10-10 10:36:45 -0300

    obter apenas tarefas em aberto

--- sistema/web/views/demanda/tarefa_card_view.py
+++ sistema/web/views/demanda/tarefa_card_view.py
@@ -1,7 +1,8 @@
 from flask import render_template_string
 from flask_login import login_required
+import sqlalchemy
 
-from sistema.model.entidades.tarefa import Tarefa
+from sistema.model.entidades.tarefa import StatusTarefa, Tarefa
 from sistema.web import renderizacao
 
 
@@ -10,9 +11,16 @@ def setup_views(app, db):
     @login_required
     def tarefa_card_view(demanda_id: int):
         tarefas = (
-            db.query(Tarefa)
-            .filter(Tarefa.demanda_id == demanda_id)
-            .order_by(Tarefa.data_hora)
+            db.execute(
+                sqlalchemy.select(Tarefa)
+                .where(
+                    Tarefa.demanda_id == demanda_id,
+                    Tarefa.status == StatusTarefa.EM_ABERTO,
+                )
+                .order_by(Tarefa.data_hora)
+            )
+            .scalars()
+            .all()
         )
         return renderizacao.renderizar_sequencia_tarefas_card(tarefas)
 
