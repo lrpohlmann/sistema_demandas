f4dcf66911cf9b7668877470359412515ff3b263
lrpohlmann <leo.pohlmann@hotmail.com>
2022-10-10 15:34:39 -0300

    validacao pagina pedida

--- sistema/servicos/pagincao.py
+++ sistema/servicos/pagincao.py
@@ -1,7 +1,7 @@
 from asyncio import Protocol
 import functools
 import math
-from typing import Sequence, TypedDict
+from typing import Sequence, TypedDict, Union
 
 
 class Paginador(Protocol):
@@ -28,9 +28,24 @@ def paginar(dados: Sequence, tamanho_pagina: int) -> Paginacao:
 def obter_pagina_dos_dados(
     dados: Sequence, tamanho_pagina: int, numero_paginas: int, pagina: int
 ) -> Sequence:
+    pagina = _validar_pagina_pedida(pagina, numero_paginas)
     if pagina == 1:
         return dados[0:tamanho_pagina]
     elif numero_paginas >= pagina:
         return dados[tamanho_pagina * (pagina - 1) : tamanho_pagina * pagina]
     else:
         return []
+
+
+def _validar_pagina_pedida(
+    pagina_pedida: Union[None, str, int], numero_de_paginas: int
+) -> int:
+    if pagina_pedida is None:
+        pagina_pedida = 1
+    elif isinstance(pagina_pedida, str):
+        pagina_pedida = int(pagina_pedida)
+
+    if pagina_pedida > numero_de_paginas:
+        return 1
+
+    return pagina_pedida
