30360764d48fdac395f7a035653ceb717f5c1d98
lrpohlmann <leo.pohlmann@hotmail.com>
2022-10-15 22:28:28 -0300

    refatoração obter_ultimas_demandas_pendentes

--- sistema/web/views/demanda/obter_ultimas_demandas_pendentes_view.py
+++ sistema/web/views/demanda/obter_ultimas_demandas_pendentes_view.py
@@ -2,7 +2,7 @@ from flask import request
 from flask_login import login_required
 
 from sistema.model.entidades import Demanda
-from sistema import servicos
+from sistema.servicos import pagincao
 from sistema.web import renderizacao
 
 
@@ -18,28 +18,17 @@ def setup_views(app, db):
             .all()
         )
 
-        paginas = servicos.paginar(demandas, 10)
-
-        pagina_requerida = (
-            int(request.args.get("pagina")) if request.args.get("pagina") else 1
+        paginas = pagincao.paginar(demandas, 10)
+        pagina_requerida = pagincao.validar_pagina_pedida(
+            request.args.get("pagina"), paginas["numero_paginas"]
         )
-        if pagina_requerida == 1 or (
-            pagina_requerida < 1 or pagina_requerida > paginas["numero_paginas"]
-        ):
-            return renderizacao.renderizar_tabela_de_demandas(
-                demandas=paginas["paginador"](1),
-                numero_paginas=paginas["numero_paginas"],
-                pagina_atual=1,
-                nome_da_view="obter_ultimas_demandas_pendentes",
-                kwargs_url={},
-            )
-        else:
-            return renderizacao.renderizar_tabela_de_demandas(
-                demandas=paginas["paginador"](pagina_requerida),
-                numero_paginas=paginas["numero_paginas"],
-                pagina_atual=pagina_requerida,
-                nome_da_view="obter_ultimas_demandas_pendentes",
-                kwargs_url={},
-            )
+
+        return renderizacao.renderizar_tabela_de_demandas(
+            demandas=paginas["paginador"](pagina_requerida),
+            numero_paginas=paginas["numero_paginas"],
+            pagina_atual=pagina_requerida,
+            nome_da_view="obter_ultimas_demandas_pendentes",
+            kwargs_url={},
+        )
 
     return app, db
