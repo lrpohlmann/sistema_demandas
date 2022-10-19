5699edc0ed9ef7a7390e0d939c88a60f8f2695b6
lrpohlmann <leo.pohlmann@hotmail.com>
2022-10-19 19:38:31 -0300

    obter form criar tipo demanda

--- sistema/web/views/demanda/criar_tipo_demanda_view.py
+++ sistema/web/views/demanda/criar_tipo_demanda_view.py
@@ -7,11 +7,11 @@ from sistema.web import renderizacao, eventos_cliente
 
 
 def setup_views(app, db):
-    @app.route("/tipo-demanda/criar", methods=["POST"])
+    @app.route("/tipo-demanda/criar", methods=["GET", "POST"])
     @login_required
     def criar_tipo_demanda_view():
         form = criar_tipo_demanda.criar_form(flask.request.form)
-        if criar_tipo_demanda.e_valido(form):
+        if flask.request.method == "POST" and criar_tipo_demanda.e_valido(form):
             dados = criar_tipo_demanda.obter_dados(form)
             db.add(TipoDemanda(dados["nome"]))
             db.commit()
