2b7254a7e6923b63f3e7efc70bdf9334adc8f3a6
lrpohlmann <leo.pohlmann@hotmail.com>
2022-10-25 17:58:16 -0300

    macro componente accordion

---
+++ test/test_web/test_template/test_template_componentes.py
@@ -0,0 +1,16 @@
+import flask
+
+from test.test_web.fixtures import WebAppFixture, web_app_com_autenticacao
+
+
+def test_renderizar_accordion(web_app_com_autenticacao: WebAppFixture):
+    with web_app_com_autenticacao.app.app_context():
+        renderizado = flask.render_template_string(
+            """{% from 'macros/geral/bootstrap/accordion.html' import accordion %} 
+            {% macro texto() %}
+            <div>{{lipsum()}}</div>
+            {% endmacro %} 
+            {{accordion('accord1', [{'titulo': 'bloco de texto', 'conteudo': texto},])}}"""
+        )
+
+    assert renderizado
