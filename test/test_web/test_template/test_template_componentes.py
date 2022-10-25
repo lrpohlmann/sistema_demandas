aadcc366dc31268f33a6deed17b640b9bd50b35a
lrpohlmann <leo.pohlmann@hotmail.com>
2022-10-25 19:19:23 -0300

    accordion aplicado

--- test/test_web/test_template/test_template_componentes.py
+++ test/test_web/test_template/test_template_componentes.py
@@ -7,10 +7,10 @@ def test_renderizar_accordion(web_app_com_autenticacao: WebAppFixture):
     with web_app_com_autenticacao.app.app_context():
         renderizado = flask.render_template_string(
             """{% from 'macros/geral/bootstrap/accordion.html' import accordion %} 
-            {% macro texto() %}
-            <div>{{lipsum()}}</div>
+            {% macro texto(n) %}
+            <div>{{lipsum(n)}}</div>
             {% endmacro %} 
-            {{accordion('accord1', [{'titulo': 'bloco de texto', 'conteudo': texto},])}}"""
+            {{accordion('accord1', [{'titulo': 'bloco de texto', 'conteudo': texto, 'kwargs': {'n': 2}},])}}"""
         )
 
     assert renderizado
