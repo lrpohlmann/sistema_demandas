3bd74dab57a4ce8552b1bceeca22e87ca1aa1bb9
lrpohlmann <leo.pohlmann@hotmail.com>
2022-11-27 19:36:52 -0300

    migrations - alembic aplicado

--- sistema/persistencia/__init__.py
+++ sistema/persistencia/__init__.py
@@ -1,19 +1,26 @@
 from importlib_metadata import metadata
 from sqlalchemy.orm import scoped_session, sessionmaker, registry
-from sqlalchemy import MetaData, create_engine, event
+from sqlalchemy import MetaData, create_engine
 from sqlalchemy.engine import Engine
+from sqlalchemy.event import listen
 
 from sistema.persistencia.orm_mapping import mapear, criar_tabelas
 
 
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
+def ativar_chave_estrangeira_sqlite(dbapi_connection, connection_record):
     cursor = dbapi_connection.cursor()
     cursor.execute("PRAGMA foreign_keys=ON")
     cursor.close()
 
 
+def desativar_chave_estrangeira_sqlite(dbapi_connection, connection_record):
+    cursor = dbapi_connection.cursor()
+    cursor.execute("PRAGMA foreign_keys=OFF")
+    cursor.close()
+
+
 def setup_persistencia(db_path: str):
+    listen(Engine, "connect", ativar_chave_estrangeira_sqlite)
     engine = create_engine(db_path, future=True)
     mapper = registry()
     metadata = MetaData(bind=engine)
